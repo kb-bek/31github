bb0611f6487d4b969b6bbb0c24e4680590f6366a
Kubanychbek Talipov <talipovkuba555@gmail.com>
2023-11-08 10:57:47 +0600

    Add new task4-3

---
+++ tasks/python/task4_3.py
@@ -0,0 +1,20 @@
+#Дано число N. Найти сумму простых делителей числа N.
+
+n = int(input())
+divisors_sum = 0
+
+prime_nums = []
+k = 0
+for i in range(2, n+1):
+    for j in range(2, i):
+        if i % j == 0:
+            k = k + 1
+    if k == 0:
+        prime_nums.append(i)
+    else:
+        k = 0
+for divisor in range(2, n):
+    if n % divisor == 0 and divisor in prime_nums:
+        divisors_sum += divisor
+
+print(divisors_sum
