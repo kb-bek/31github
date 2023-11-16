064da1c5de0c1f502f52f5ebdddd21f4b25d7d01
Kubanychbek Talipov <talipovkuba555@gmail.com>
2023-11-16 10:04:00 +0600

    Add python task

---
+++ tasks/python/task7_23.py
@@ -0,0 +1,25 @@
+length_num = int(input())
+numbers = [int(i) for i in input().split()]
+
+def average(nums):
+    summa = 0
+    for i in nums:
+        summa += i
+    return summa / len(nums)
+
+if length_num % 2 != 0:
+    mediana = sorted(numbers)[length_num//2]
+    if mediana > average(numbers):
+        print(True)
+    else:
+        print(False)
+else:
+    mediana = sorted(numbers)[length_num//2-1]
+    if mediana > average(numbers):
+        print(True)
+    else:
+        print(False)
+
+
+
+
