a736d4c9aceda83150cdd79a2e75d15eccc339c5
Kubanychbek Talipov <talipovkuba555@gmail.com>
2023-11-18 15:40:17 +0600

    Add task about variables in java

---
+++ ghl_guides/guide_for_beginners/Variables.java
@@ -0,0 +1,14 @@
+package ghl_guide;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int x = 98;
+        int y = 22;
+
+        int res = (x + 5) * y * y;
+
+        System.out.println(res);
+
+    }
+}
