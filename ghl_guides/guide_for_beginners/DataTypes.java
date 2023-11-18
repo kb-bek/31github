0576662961ca576f9aeefc8b7f9cd502adfd7ecf
Kubanychbek Talipov <talipovkuba555@gmail.com>
2023-11-18 15:40:57 +0600

    Add task data types in java

---
+++ ghl_guides/guide_for_beginners/DataTypes.java
@@ -0,0 +1,20 @@
+package ghl_guide;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int num = 4340;
+        byte byteNum = 30;
+        short shortNum = 432;
+        long longNum = 8765443;
+
+        double dblNum = 435.32;
+        float flNum = 765.87f;
+
+        boolean blNum = true;
+
+        char symbol = '&';
+        String someWord = "Coffee";
+
+    }
+}
