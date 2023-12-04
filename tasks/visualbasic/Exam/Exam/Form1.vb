ff20882726039de3840b7d0b42d4cd8b58638424
Kubanychbek Talipov <talipovkuba555@gmail.com>
2023-12-04 10:48:03 +0600

    Add visualbasic exam project

---
+++ tasks/visualbasic/Exam/Exam/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Linq.Expressions
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim adi As String
+        Dim parol As Integer
+        Dim girilenAd As String
+        Dim girilenParol As Integer
+
+        adi = "bilisim"
+        parol = 12345
+        girilenAd = TextBox1.Text
+        girilenParol = TextBox2.Text
+
+        If girilenAd = adi And girilenParol = parol Then
+            Button2.Text = "Giris basarili"
+            Button2.BackColor = Color.Green
+        Else
+            Button2.Text = "Giris basarisiz"
+            Button2.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+End Class
